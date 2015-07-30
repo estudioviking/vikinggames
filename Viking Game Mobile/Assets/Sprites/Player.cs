cdae04d4f5d887eef12051912400c60b88c3ea2e
sergiofffilho <sergiofffilho@gmail.com>
2015-07-30 16:30:53 -0300

    Primeiro Commit

---
+++ Viking Game Mobile/Assets/Sprites/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	private int vidaPlayer;
+	private int powerPlayer;
+	private int levelPlayer;
+
+
+	private int dano_sem_arma;
+	private int dano_especial_arma;
+	private int dano_arma;
+	private int dano_total;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
